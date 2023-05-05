d88c1b52880b2dd1eadf1e06bd36de0e0a4f699c
Azat <aelchikushuulu@gmail.com>
2023-05-05 16:23:01 +0600

    Initial commit

---
+++ src/main/java/com/github/aza06_02/springandmockito/SpringAndMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.github.aza06_02.springandmockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndMockitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAndMockitoApplication.class, args);
+    }
+
+}
